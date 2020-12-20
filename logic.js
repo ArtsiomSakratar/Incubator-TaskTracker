c609978e79da39bbc51a53e24799492ab4af3c80
Artsiom <artsiom.sakratar@gmail.com>
2020-12-20 15:33:02 +0300

    first

---
+++ logic.js
@@ -0,0 +1,15 @@
+let addTask = document.querySelector('form');
+
+let title;
+let text;
+let priority;
+
+addTask.onsubmit = function () {
+    document.querySelector('#inputTitle').value;
+    document.querySelector('#inputText').value;
+    
+    console.log('hi');
+};
+
+console.log(title);
+console.log(text);
