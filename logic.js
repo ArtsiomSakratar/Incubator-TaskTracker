a4ac0de2af73ddd8d277ce77bfffef501fe216f2
Artsiom <artsiom.sakratar@gmail.com>
2020-12-20 15:45:43 +0300

    test

--- logic.js
+++ logic.js
@@ -1,15 +1,23 @@
-let addTask = document.querySelector('form');
+let addForm = document.querySelector('#addTaskForm');
+let addTaskButton = document.querySelector("#addTaskForm");
 
 let title;
 let text;
 let priority;
 
-addTask.onsubmit = function () {
-    title = document.querySelector('#inputTitle').value;
-    text = document.querySelector('#inputText').value;
-    
-    console.log('hi');
+addForm.onsubmit = function () {
+  title = document.querySelector("#inputTitle").value;
+  text = document.querySelector("#inputText").value;
+
+  console.log("hi");
 };
+
+addTaskButton.onclick = function () {
+    title = document.querySelector("#inputTitle").value;
+    text = document.querySelector("#inputText").value;
+
+    console.log("hi");
+}
 
 console.log(title);
 console.log(text);
